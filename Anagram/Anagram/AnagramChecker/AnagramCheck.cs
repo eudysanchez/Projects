2aee0eefdadebb5a169fde7c9ce8dd15161d4366
Eudy <eudysanchez@gmail.com>
2016-07-14 16:59:38 -0600

    -Add Aperations to main class -Fix workflow

---
+++ Anagram/Anagram/AnagramChecker/AnagramCheck.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Anagram.AnagramChecker
+{
+    public class AnagramCheck : IAnagramCheck
+    {
+        public AnagramCheck() { }
+
+        public bool IsAnagram(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
+                return false;
+            if (s1.Length != s2.Length)
+                return false;
+
+            foreach (char c in s2)
+            {
+                int indexOfLetter = s1.IndexOf(c);
+                if (indexOfLetter  >= 0)
+                    s1 = s1.Remove(indexOfLetter, 1);
+                else
+                    return false;
+            }
+
+            return string.IsNullOrEmpty(s1);
+        }
+    }
+}
--- Anagram/Anagram/Program.cs
+++ Anagram/Anagram/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Anagram.AnagramChecker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,25 @@ namespace Anagram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter First word");
+            var input1 =  Console.ReadLine();
+
+            Console.WriteLine("Enter Second word");
+            var input2 = Console.ReadLine();
+
+            string result;
+            IAnagramCheck check = new AnagramCheck();
+
+            if (check.IsAnagram(input1, input2))
+            {
+                result = "Are Anagrams";
+            }
+            else
+                result = "Are Not Anagrams";
+
+            Console.WriteLine(input1 + " " + input2 + " " + result);
+
+            Console.ReadKey();
         }
     }
 }
