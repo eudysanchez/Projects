cba5033a746201979eb86cc9eeaac2cf6dee2030
Eudy <eudysanchez@gmail.com>
2016-07-14 23:46:43 -0600

    adding largest paralindrome

---
+++ Palindrome/Palindrome/PalindromeResources/PalindromeFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Reference: https://miafish.wordpress.com/2015/01/23/leetcode-oj-c-longest-palindromic-substring/
+namespace PalindromeResources
+{
+    public class PalindromeFinder : IPalindromeFinder
+    {
+        public PalindromeFinder(){ }
+
+        public string FindLargestPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return String.Empty;
+            var result = string.Empty;
+            var reslen = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var p1 = expandAroundCenter(input, i, i);
+                if (p1.Length > reslen)
+                {
+                    reslen = p1.Length;
+                    result = p1;
+                }
+
+                var p2 = expandAroundCenter(input, i, i + 1);
+                if (p2.Length > reslen)
+                {
+                    reslen = p2.Length;
+                    result = p2;
+                }
+            }
+
+            return result;
+        }
+
+        private string expandAroundCenter(string s, int i, int j)
+        {
+            while (i >= 0 && j < s.Length && s[i] == s[j])
+            {
+                i--;
+                j++;
+            }
+
+            return s.Substring(i + 1, j - i - 1);
+        }
+    }
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using PalindromeResources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPalindromeFinder palindrome = new PalindromeFinder();
+
+            Console.WriteLine("Enter Palindrome");
+            var input = Console.ReadLine();
+
+            var result = palindrome.FindLargestPalindrome(input);
+
+            Console.WriteLine("This is the Longest Palindrome: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
